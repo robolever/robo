1002082cf4aaea526e3e480d16aa86ea6df6bc71
Streple Duck <streple@streple>
2025-10-24 04:59:53 -0400

    links

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 dbPassword33 = 'mongodb://localhost:27017/mern2';
-dbPassword = 'mongodb+srv://entryanalytica:CrypticPass12@cluster.btejtgz.mongodb.net/EntryanalyticaUsers'
+dbPassword = 'mongodb+srv://Robolever:RoboPass.00@cluster0.somca5z.mongodb.net/RoboleverUsers'
 module.exports = {
     mongoURI: dbPassword
 };
