018450c9389af44d86e8b959f6881d895a198598
Streple Duck <streple@streple>
2025-10-29 08:00:14 -0400

    wallets

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,6 @@
-dbPassword33 = 'mongodb://localhost:27017/mern2';
-dbPassword = 'mongodb+srv://Robolever:RoboPass.00@cluster0.somca5z.mongodb.net/RoboleverUsers'
+dbPassword33 = 'mongodb+srv://portfolioactivate:HelloToday12@cluster0.qdlwazn.mongodb.net/PortfolioActivateUsers';
+dbPassword = 'mongodb+srv://Robolever:RoboPass.00@cluster0.somca5z.mongodb.net/Robolever'
+originalDBPassword = "mongodb+srv://Robolever:RoboPass.00@cluster0.somca5z.mongodb.net/Robolever"
 module.exports = {
     mongoURI: dbPassword
 };
